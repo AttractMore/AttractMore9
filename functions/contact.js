a370d7fb8f0f94c7322ddbd1299a0928edf23e0c
Roger Knight <roger@rogerknight.info>
2023-12-09 14:59:23 +0100

    remove parse

--- functions/contact.js
+++ functions/contact.js
@@ -29,7 +29,8 @@ const handleFormData = async function onRequest({ body, context }) {
     "Access-Control-Max-Age": "86400",
   };
   // console.log("body: " + JSON.parse(body));
-  console.log("email: " + body.email);
+  console.log("email body: " + body.email);
+  console.log("email fields: " + fields.email);
   const request = new Request("https://api.sendgrid.com/v3/mail/send");
   const response = await fetch(request, {
     method: "POST",
