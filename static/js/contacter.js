1200e0bc9d7f7a23ffe8826ca28be1707244417d
Roger Knight <986557+AttractMore@users.noreply.github.com>
2023-12-23 22:39:57 +0100

    Delete static/js/contacter.js

--- static/js/contacter.js
+++
@@ -1,7 +0,0 @@
-import * as contact from "../functions/contact.js";
-
-/**
- * Submit form using Cloudflare function
- */
-const enquiryForm = document.getElementById("enquiryform");
-enquiryForm.addEventListener("submit", contact);
