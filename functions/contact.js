455e231584824c77dd9a9f662984d7b19785116e
Roger Knight <roger@rogerknight.info>
2023-12-23 14:31:43 +0100

    rejig js ot export default

--- functions/contact.js
+++ functions/contact.js
@@ -1,36 +1,38 @@
-export async function onRequestPost(context) {
-  const body = await context.request.formData();
+export default {
+  async fetch(request, env, context) {
+    const body = await context.request.formData();
 
-  const { name, email, message, consent } = Object.fromEntries(body);
+    const { name, email, message, consent } = Object.fromEntries(body);
 
-  const response = await fetch(context.env.ZOHO_WEBHOOK, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      subject: "AM9 Website Contact Form",
-      message: `
-      <html>
-      <body>
-      <h1>Contact request from Attractmore website.</h1>
-        <p>Name: ${name}</p>
-        <p>Email: ${email}</p>
-        <p>Message: ${message}</p>
-        <p>Consent: ${consent}</p>
-      </body>
-    </html>
-      `,
-    }),
-  });
-  let replyBody;
-  if (response.ok) {
-    return Response.redirect("https://attractmore9.pages.dev/thanks/");
-  } else {
-    console.error(response.status, response.statusText);
-    replyBody = { "success": false, "message": response.statusText };
-    return new Response(JSON.stringify(replyBody), {
-      status: response.status,
+    const response = await fetch(context.env.ZOHO_WEBHOOK, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        subject: "AM9 Website Contact Form",
+        message: `
+          <html>
+          <body>
+          <h1>Contact request from Attractmore website.</h1>
+          <p>Name: ${name}</p>
+          <p>Email: ${email}</p>
+          <p>Message: ${message}</p>
+          <p>Consent: ${consent}</p>
+          </body>
+          </html>
+        `,
+      }),
     });
-  }
-}
+    let replyBody;
+    if (response.ok) {
+      return Response.redirect("https://attractmore9.pages.dev/thanks/");
+    } else {
+      console.error(response.status, response.statusText);
+      replyBody = { "success": false, "message": response.statusText };
+      return new Response(JSON.stringify(replyBody), {
+        status: response.status,
+      });
+    }
+  },
+};
--- static/js/contacter.js
+++ static/js/contacter.js
@@ -1,12 +1,7 @@
 import { contact } from "../functions/contact.js";
 
-function makeContact(event) {
-  contact();
-  event.preventDefault();
-}
-
 /**
  * Submit form using Cloudflare function
  */
 const enquiryForm = document.getElementById("enquiryform");
-enquiryForm.addEventListener("submit", makeContact);
+enquiryForm.addEventListener("submit", contact);
