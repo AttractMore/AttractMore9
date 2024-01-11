9028ab999e786842eaa55eda216ea27117b12356
Roger Knight <roger@rogerknight.info>
2024-01-11 22:44:18 +0100

    menu mainly

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -19,10 +19,8 @@ for (var i = 0, linksLength = links.length; i < linksLength; i++) {
  * @source - https://web.dev/articles/website-navigation
  */
 const nav = document.querySelector(".site-head__nav");
-const list = nav.querySelector("ul");
-const burgerClone = document.querySelector("#burger-template").content.cloneNode(true);
-
-const button = burgerClone.querySelector("button");
+const head = document.querySelector(".site-head__inner");
+const button = head.querySelector(".nav-burger");
 button.addEventListener("click", (e) => {
   const isOpen = button.getAttribute("aria-expanded") === "false";
 
@@ -30,12 +28,10 @@ button.addEventListener("click", (e) => {
   nav.setAttribute("aria-expanded", isOpen);
 });
 
-nav.addEventListener("keyup", (e) => {
+body.addEventListener("keyup", (e) => {
   if (e.code === "Escape") {
     button.setAttribute("aria-expanded", false);
     nav.setAttribute("aria-expanded", false);
     button.focus();
   }
 });
-
-nav.insertBefore(burgerClone, list);
