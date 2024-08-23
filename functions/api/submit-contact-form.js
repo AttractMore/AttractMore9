c039a4725529e86dacf54226556a98351bd10993
Roger Knight <roger@rogerknight.info>
2024-08-23 17:09:35 +0200

    SET up required fields

--- functions/api/submit-contact-form.js
+++ functions/api/submit-contact-form.js
@@ -2,9 +2,13 @@
  * POST /api/submit-contact-form
  *
  */
+import { Resend } from "resend";
+
 export async function onRequestPost(context) {
   try {
-    let input = await context.request.formData(); // Convert FormData to JSON
+    let input = await context.request.formData();
+
+    // Convert FormData to JSON
     // NOTE: Allows multiple values per key
     let output = {};
     for (let [key, value] of input) {
@@ -15,9 +19,35 @@ export async function onRequestPost(context) {
         output[key] = [].concat(tmp, value);
       }
     }
-    let pretty = JSON.stringify(output, null, 2);
-    return new Response(pretty, { headers: { "Content-Type": "application/json;charset=utf-8" } });
+    // output: {
+    //   name: 'Jane Doe',
+    //   'contact-name': '',
+    //   email: 'jane@doe.com',
+    //   message: 'this is my message'
+
+    const honeypot = output["contact-name"];
+    // Return early with pretend confirmation if bot hit honeypot
+    if (honeypot !== "") {
+      return Response.redirect("https://attractmore.uk/thanks/", 303);
+    }
+
+    // Using text instead of email so that I don't need to sanitize it
+    const resend = new Resend(context.env.RESEND_API_KEY);
+    const { data, error } = await resend.emails.send({
+      from: context.env.SENDER_EMAIL,
+      reply_to: output.email,
+      to: context.env.RECIPIENT_EMAIL,
+      subject: `[ATTRACTMORE] Contact form request`,
+      text: output.message,
+    });
+    console.log({ data, error });
+
+    if (error) {
+      return Response.redirect("https://attractmore.uk/404", 303);
+    } else {
+      return Response.redirect("https://attractmore.uk/thanks/", 303);
+    }
   } catch (err) {
-    return new Response("Error parsing JSON content", { status: 400 });
+    return Response.redirect("https://attractmore.uk/404?error=json_parsing", 303);
   }
 }
