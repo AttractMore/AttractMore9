76a153e1afbf7487dbc4343289f5ede61a3daff2
Roger Knight <roger@rogerknight.info>
2024-08-26 13:13:21 +0200

    Remove mail check

--- static/sw2.js
+++ static/sw2.js
@@ -14,8 +14,8 @@ limitations under the License.
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "precache-v145";
-const RUNTIME = "runtime-v145";
+const PRECACHE = "precache-v146";
+const RUNTIME = "runtime-v146";
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
