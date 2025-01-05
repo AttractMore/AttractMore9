f793ead6a4a959da6c678697358266e0287e3206
Roger Knight <roger@rogerknight.info>
2025-01-05 08:34:45 +0100

    reply_to email in []

--- functions/api/submit-contact-form.js
+++ functions/api/submit-contact-form.js
@@ -34,7 +34,7 @@ export async function onRequestPost(context) {
     const resend = new Resend(context.env.RESEND_API_KEY);
     const { data, error } = await resend.emails.send({
       from: context.env.SENDER_EMAIL,
-      reply_to: `${output.email}`,
+      reply_to: [`${output.email}`],
       to: context.env.RECIPIENT_EMAIL,
       subject: `[ATTRACTMORE] Contact form request`,
       text: `
@@ -42,7 +42,6 @@ export async function onRequestPost(context) {
       Email: ${output.email}, 
       Message: ${output.message}`,
     });
-    console.log({ data, error });
 
     if (error) {
       return Response.redirect("https://attractmore.uk/404", 303);
