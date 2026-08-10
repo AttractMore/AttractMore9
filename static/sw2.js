5703895570153f9da75fa724f32cf9faf25a43c8
Roger Knight <roger@rogerknight.info>
2026-08-10 09:27:34 +0200

    Remove BBN

--- static/sw2.js
+++ static/sw2.js
@@ -14,8 +14,8 @@ limitations under the License.
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "precache-v175";
-const RUNTIME = "runtime-v175";
+const PRECACHE = "precache-v176";
+const RUNTIME = "runtime-v176";
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
