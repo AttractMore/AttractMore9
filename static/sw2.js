2fdc0a24d428ddf244ce0196c4f1a5d30cd4e9a9
Roger Knight <roger@rogerknight.info>
2026-08-10 09:01:05 +0200

    adjust for FormSpark

--- static/sw2.js
+++ static/sw2.js
@@ -14,8 +14,8 @@ limitations under the License.
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "precache-v172";
-const RUNTIME = "runtime-v172";
+const PRECACHE = "precache-v173";
+const RUNTIME = "runtime-v173";
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
