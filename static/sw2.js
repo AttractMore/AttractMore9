9bd636a26dc13227a7462ca51dda99dd3108422e
Roger Knight <roger@rogerknight.info>
2026-08-10 08:52:33 +0200

    bump sw

--- static/sw2.js
+++ static/sw2.js
@@ -14,8 +14,8 @@ limitations under the License.
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "precache-v171";
-const RUNTIME = "runtime-v171";
+const PRECACHE = "precache-v172";
+const RUNTIME = "runtime-v172";
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
